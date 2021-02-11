7a7732dad173763e503f557cf30970bbfee587b8
Ramiro Elisei <ramiroelisei@gmail.com>
2021-02-11 11:02:27 +0100

    vistazo-rating

---
+++ (M7)Rating2/Nivel-IIIIIII/indexjs.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+// document.getElementById('Input').checked = false;
+// var radios = document.getElementsByTagName('input');
+// for(i=0; i<radios.length; i++) {
+//     radios[i].onclick = function(e) {
+//         if(e.ctrlKey || e.metaKey) {
+//             this.checked = false;
+//         }
+//     }
+// }
+
+
+
+
+// function mouseout(){
+//     var removeStars = document.getElementsByClassName('star');
+//     for(var i = 0; i < removeStars.length; i++){
+//         removeStars[i].style.color('#2ce679');
+//     }
+// }
+
+
+// removeStars.item(i).classList.remove('#2ce679');
+
+
+// if(type === 'mouseout'){
+//     Element.classlist.remove("yellow");
+// }
